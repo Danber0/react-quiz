175504525cf19d17f7b82419447c0d8dc269c575
Danber <beraguzen.danil@mail.ru>
2021-11-22 21:55:10 +0300

    react-quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.scss';
+import { questions } from './heplers/questions';
+
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="quiz">
+        <div className="quiz__menu">
+          {showScore ? (
+            <div className="score-section">
+              Поздравляю! Ты правильно ответили на {(score / questions.length) * 100} % вопросов
+            </div>
+          ) : (
+            <>
+              <div className="question-section">
+                <div className="question-count">
+                  <span>Номер вопроса {currentQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className="question-text">{questions[currentQuestion].questionText}</div>
+              </div>
+              <div className="answer-section">
+                {questions[currentQuestion].answerOptions.map((answerOption) => (
+                  <button
+                    className="button-answer"
+                    onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                    {answerOption.answerText}
+                  </button>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/heplers/questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+  {
+    questionText: 'Какая сталица франции?',
+    answerOptions: [
+      { answerText: 'Нью-йорк', isCorrect: false },
+      { answerText: 'Лондон', isCorrect: false },
+      { answerText: 'Париж', isCorrect: true },
+      { answerText: 'Дублин', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Кто директор в компании тесла?',
+    answerOptions: [
+      { answerText: 'Джеф Безос', isCorrect: false },
+      { answerText: 'Илон Маск', isCorrect: true },
+      { answerText: 'Бил Гейтс', isCorrect: false },
+      { answerText: 'Тони старк', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Какая компания создала Iphone?',
+    answerOptions: [
+      { answerText: 'Apple', isCorrect: true },
+      { answerText: 'Intel', isCorrect: false },
+      { answerText: 'Amazon', isCorrect: false },
+      { answerText: 'Microsoft', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Сколько книг в гарри потере?',
+    answerOptions: [
+      { answerText: '1', isCorrect: false },
+      { answerText: '4', isCorrect: false },
+      { answerText: '6', isCorrect: false },
+      { answerText: '7', isCorrect: true },
+    ],
+  },
+];
